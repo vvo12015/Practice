6df23caa39059308225e3ee4bdc076bf1118c4ee
Vrakin Valentin <vvo12015@gmail.com>
2016-05-09 12:44:15 +0300

    modified SetZero

--- src/test/java/workshop2/SetZeroTest.java
+++ src/test/java/workshop2/SetZeroTest.java
@@ -3,17 +3,43 @@ package workshop2;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class SetZeroTest {
+
     @Test
-    public void testSetZero() throws Exception {
+    public void testSetZeroPositive() throws Exception {
         int input = 6;
         int result = 4;
 
         SetZero setZero = new SetZero();
 
-        Assert.assertEquals("equals", result, setZero.set(input, 1));
+        Assert.assertEquals("normally value", result, setZero.set(input, 1));
     }
 
+    @Test
+    public void testSetOne() throws Exception {
+
+        int input = -1;
+        int result = 0;
+
+        SetZero setZero = new SetZero();
+
+        Assert.assertEquals("check on one", result, setZero.set(input, 31));
+    }
+
+    @Test
+    public void testSetZeroNegative() throws Exception {
+
+        int input = -1;
+        int numberBit = 31;
+        int result = -1073741825;
+
+        SetZero setZero = new SetZero();
+
+        Assert.assertEquals("check min", result, setZero.set(input, numberBit));
+    }
 }
