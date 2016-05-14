6e295ccd5d94cec0d55fc364a9c9be9b368acf35
vvo12 <vvo12015@gmail.com>
2016-05-14 23:11:34 +0300

    added workshop3 GnomeFood

---
+++ src/main/java/workshop3/GnomeFood.java
@@ -0,0 +1,11 @@
+package workshop3;
+
+/**
+ * Created by vvo12 on 14.05.2016.
+ */
+public class GnomeFood {
+
+    public int[] find(int[] gnames, int[] portions) {
+
+    }
+}
--- src/test/java/workshop3/BinaryHeapTest.java
+++ src/test/java/workshop3/BinaryHeapTest.java
@@ -4,9 +4,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by vvo12 on 14.05.2016.
- */
 public class BinaryHeapTest {
     @Test
     public void insert() throws Exception {
